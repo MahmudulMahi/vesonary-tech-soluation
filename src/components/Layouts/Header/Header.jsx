bd81ac62f5317467ed63617d3d1457063711f870
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-10-26 01:23:30 +0600

    work header section

--- src/components/Layouts/Header/Header.jsx
+++ src/components/Layouts/Header/Header.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
+
 import { NavLink } from 'react-router-dom';
 
+
 const Header = () => {
+
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden" >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
             <li><NavLink to="/">Home</NavLink></li>
             <li>
-              <a>Parent</a>
+              <a to="services">services</a>
               <ul className="p-2">
                 <li><a>Submenu 1</a></li>
                 <li><a>Submenu 2</a></li>
               </ul>
             </li>
-            <li><a>Item 3</a></li>
+            <li><NavLink to="/team">Team</NavLink></li>
+            <li><NavLink to="/project">Project</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
           </ul>
         </div>
         <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
@@ -40,10 +45,21 @@ const Header = () => {
           <li><NavLink to="/project">Project</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/contact">Contact</NavLink></li>
-         
+
+
         </ul>
+
       </div>
+
       <div className="navbar-end">
+        <div className='flex items-center relative  '>
+          <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+
+          <div className='absolute right-5'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+          </div>
+
+        </div>
         <a className="btn">Button</a>
       </div>
     </div>
